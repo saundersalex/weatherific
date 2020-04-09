aad6201a5fd59d9b3fe8ac516e01fc204d9c9ec7
Alex Saunders <saundersalex@gmail.com>
2020-04-09 15:14:41 -0700

    * Added Emotion for CSS-in-JS styling. * Added Normalize.css for consistent CSS reset across browsers. * Updated index.html and manifest.json with more appropriate information. * Added Barlow font face, to be used as the primary font style.

--- src/Weatherific.js
+++ src/Weatherific.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Weatherific = () => {
-  return (
+
+  const [location, setLocation] = useState('Vancouver');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [units, setUnits] = useState('metric');
+  const [weatherData, setWeatherData] = useState(null);
+  
+  return <>
     <h1>Hello, Weatherific!</h1>
-  );
+  </>;
 };
 
 export default Weatherific;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Global, css} from '@emotion/core';
+
 import Weatherific from './Weatherific';
+
+// Gives us a nice CSS reset for a clean slate across different browsers.
+import 'normalize.css';
+
+const globalStyles = css`
+  body {
+    font-family: Barlow, sans-serif;
+    background-color: #eee;
+    padding: 20px;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <Weatherific />
   </React.StrictMode>,
   document.getElementById('root')
